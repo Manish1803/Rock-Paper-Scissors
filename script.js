01de9fea74be51226442029bc4b85eb61c535515
Manish Patil <patilmanishp18@gmail.com>
2024-08-26 16:21:41 +0530

    Updates files
    add localStorage to display/handle scores

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Button and modal elements for showing rules
 const btnRules = document.querySelector(".btn-rules");
 const btnClose = document.querySelector(".btn-close");
 const modal = document.querySelector(".rules-modal");
 
+// Sections for game and result display
 const gameSec = document.querySelector(".game");
 const resultSec = document.querySelector(".result");
 
+// Elements related to the game choices and results
 const choices = document.querySelectorAll(".choice");
 const picked = document.querySelectorAll(".picked");
 
@@ -35,6 +38,7 @@ const CHOICES = [
   },
 ];
 
+// Add event listeners to each choice button
 choices.forEach((btn) =>
   btn.addEventListener("click", () => {
     const choiceName = btn.dataset.choice;
@@ -43,6 +47,7 @@ choices.forEach((btn) =>
   })
 );
 
+// Function to handle the user's choice and randomly select the computer's choice
 function choose(choice) {
   const rand = Math.floor(Math.random() * CHOICES.length);
   const pcChoice = CHOICES[rand];
@@ -50,6 +55,7 @@ function choose(choice) {
   displayWinner([choice, pcChoice]);
 }
 
+// Function to display the result of the game
 function displayResult(result) {
   picked.forEach((pick, idx) => {
     setTimeout(() => {
@@ -65,14 +71,21 @@ function displayResult(result) {
   resultSec.classList.toggle("hidden");
 }
 
+// Function to check is the winner
 function isWinner(result) {
   return result[0].beats === result[1].name;
 }
 
-// Update score
+// Reset scores if the tab is not just reloaded
+if (!sessionStorage.getItem("reloaded")) {
+  localStorage.setItem("scores", JSON.stringify({ user: 0, computer: 0 }));
+} else {
+  sessionStorage.removeItem("reloaded");
+}
 
+// Function to handle scores
 function handleScores(result = null) {
-  const scoresJSON = sessionStorage.getItem("scores");
+  const scoresJSON = localStorage.getItem("scores");
   const scores = scoresJSON ? JSON.parse(scoresJSON) : { user: 0, computer: 0 };
 
   if (result) {
@@ -82,7 +95,7 @@ function handleScores(result = null) {
       scores.computer += 1;
     }
 
-    sessionStorage.setItem("scores", JSON.stringify(scores));
+    localStorage.setItem("scores", JSON.stringify(scores));
   }
 
   computerScore.innerText = scores.computer;
@@ -91,19 +104,25 @@ function handleScores(result = null) {
 
 handleScores();
 
+// Function to determine and display the winner
 function displayWinner(result) {
   setTimeout(() => {
     const userWins = isWinner(result);
     const compWins = isWinner(result.reverse());
 
-    if (userWins) {
+    if (result[0].name === result[1].name) {
+      resultTitle.innerHTML = `TIE UP`;
+      btnPlayAgain.innerHTML = "Replay";
+
+      btnNext.classList.add("hidden");
+    } else if (userWins) {
       resultTitle.innerHTML = `You win<br>
       <span>Against PC</span>`;
       btnPlayAgain.innerHTML = "Play Again";
       picked[0].classList.toggle("winner");
 
       handleScores("user");
-      btnNext.classList.toggle("hidden");
+      btnNext.classList.remove("hidden");
     } else if (compWins) {
       resultTitle.innerHTML = `You Lose<br>
       <span>Against PC</span>`;
@@ -112,11 +131,6 @@ function displayWinner(result) {
 
       handleScores("comp");
       btnNext.classList.add("hidden");
-    } else {
-      resultTitle.innerHTML = `TIE UP`;
-      btnPlayAgain.innerHTML = "Replay";
-
-      btnNext.classList.add("hidden");
     }
 
     resultWinner.classList.toggle("hidden");
@@ -124,10 +138,19 @@ function displayWinner(result) {
   }, 1000);
 }
 
+// Function to handle the "Play Again" button
 function playAgain() {
   gameSec.classList.toggle("hidden");
   resultSec.classList.toggle("hidden");
-  btnNext.classList.add("hidden");
+
+  // Next button
+  const userScore = playerScore.innerText;
+  const compScore = computerScore.innerText;
+  if (userScore > compScore) {
+    btnNext.classList.remove("hidden");
+  } else {
+    btnNext.classList.add("hidden");
+  }
 
   picked.forEach((pick) => {
     pick.innerHTML = "";
@@ -139,6 +162,7 @@ function playAgain() {
   resultSec.classList.toggle("show-winner");
 }
 
+// Event listeners for modal display and reset actions
 btnPlayAgain.addEventListener("click", playAgain);
 
 btnNext.addEventListener("click", () => {
@@ -150,13 +174,16 @@ reset.addEventListener("click", () => {
   playAgain();
 });
 
-/**
- * SHOW/HIDE MODAL
- */
-
+// Event listeners for showing/hiding the rules modal
 btnRules.addEventListener("click", () => {
   modal.classList.toggle("show-modal");
 });
 btnClose.addEventListener("click", () => {
   modal.classList.toggle("show-modal");
+});
+
+// Storage reload
+
+window.addEventListener("beforeunload", () => {
+  sessionStorage.setItem("reloaded", "true");
 });
